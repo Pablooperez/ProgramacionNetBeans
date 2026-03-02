354d6cef9104134c5038ab1194022c2ecaf25e7e
pablo pérez <pablo.perez.martinez99@gmail.com>
2026-03-02 13:45:48 +0100

    ultima modificacion

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -19,6 +19,7 @@ public class HolaMundo {
         System.out.println("Hola Mundo");
         System.out.println("Otro saludo");
         System.out.println(contador);
+        System.out.println("Como estas");
     }
     
     
