538cf001fcf28666d5fb5082a3da51a78ef06ff5
pablo pérez <pablo.perez.martinez99@gmail.com>
2026-03-09 13:45:49 +0100

    Nuevo

---
+++ TorresHanoi/src/torreshanoi/Disco.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package torreshanoi;
+
+import javax.swing.JButton;
+
+
+// Constructores
+public class Disco extends JButton {
+    
+}
+
+//Set Get Is
+
+//Métodos
