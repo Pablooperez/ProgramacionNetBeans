766d233bcbe591a1a199e04f8ea3463cb75e98ed
Pablo <pablo.perez.martinez99@gmail.com>
2026-03-23 19:30:47 +0100

    Nuevo proyecto de Gestor de tareas

---
+++ GestorTareas/src/gestortareas/Tarea.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestortareas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author casa
+ */
+public class Tarea {
+   
+    //Atributos
+    
+    public enum Prioridad {
+        ALTA, MEDIA, BAJA
+    }
+    
+    private String nombre;
+    private Prioridad prioridad;
+    private boolean completada;
+    private LocalDate fechaCreacion;
+    
+    // Constructores
+    
+    public Tarea(String snombre, Prioridad prioridad1){
+        this.nombre=snombre;
+        this.prioridad=prioridad1;
+        this.completada=false;
+        this.fechaCreacion=LocalDate.now();
+    }
+    
+    //Getter and Setter
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String snombre){
+        this.nombre=snombre;
+    }
+    
+    public Prioridad getPrioridad(){
+        return this.prioridad;
+    }
+    
+    public void setPrioridad(Prioridad pprioridad){
+        this.prioridad=pprioridad;
+    }
+    
+    public boolean isCompletada(){
+        return this.completada;
+    }
+    
+    public void setCompletada(boolean bcompletada){
+        this.completada=bcompletada;
+    }
+    
+    public LocalDate getFecha(){
+        return this.fechaCreacion;
+    }
+    
+    //Métodos
+    
+    public void marcarComoCompletada(){
+        if (!this.completada) {
+            this.completada=true;
+            System.out.println("Tarea completada: " + this.nombre);
+        }
+        
+    }
+    
+    public void marcarComoIncompleta(){
+        if (this.completada) {
+            this.completada=false;
+            System.out.println("Tarea incompleta: " + this.nombre);
+        }
+    }
+    
+    public void cambiarPrioridad(Prioridad nueva){
+        if (nueva==this.prioridad) {
+            System.out.println("Error, misma prioridad.");
+        }else{
+            this.prioridad=nueva;
+            System.out.println("Prioridad cambiada: " + this.nombre + " -- " + this.prioridad);
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return "[Tarea] " + this.nombre + " - " + this.prioridad + " - " + (this.completada ? "Completada" : "Pendiente") 
+                + " -  " + this.fechaCreacion;
+    }
+    
+    
+    
+    
+}
